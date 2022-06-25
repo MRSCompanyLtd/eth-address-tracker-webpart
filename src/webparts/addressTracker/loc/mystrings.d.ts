4d23f912e4557ffc5fe5ff608d7d73dda1f9b908
dperk6 <daniel@perklin.ca>
2022-06-25 18:35:35 -0400

    init

---
+++ src/webparts/addressTracker/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IAddressTrackerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'AddressTrackerWebPartStrings' {
+  const strings: IAddressTrackerWebPartStrings;
+  export = strings;
+}
