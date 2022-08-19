66fb8e930bc2a983472c4094e9568f08312e107b
dperk6 <daniel@perklin.ca>
2022-08-19 17:30:22 -0400

    interface

--- src/interfaces/ITransaction.ts
+++ src/interfaces/ITransaction.ts
@@ -2,6 +2,7 @@ export interface ITransaction {
     asset: string;
     blockNumber: number;
     category: string;
+    contract?: string;
     erc721TokenId?: string;
     erc1155Metadata?: any;
     from: string;
